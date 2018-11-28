21359f4448ba930d3aee475c461014be4f642f6b
Thackery Archuletta <t.archuletta@utah.edu>
2018-11-27 17:11:14 -0700

    This potentially won't be necessary. Currently being used though.

---
+++ decision_tree.py
@@ -0,0 +1,8 @@
+
+
+class DecisionTree(object):
+    def __init__(self):
+        self.search_param = None
+        self.proceed = None
+        self.no_jss_id = None
+        self.needs_enrollment = None
