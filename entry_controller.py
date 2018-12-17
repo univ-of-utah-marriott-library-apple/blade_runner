83511f2d723b86aa7187d2b5f370c07735177cb4
Thackery Archuletta <t.archuletta@utah.edu>
2018-12-17 16:24:05 -0700

    - Renamed file and class. - Fixed a bug caused by binding a function to a button. Since the binding sends an event, the lambda must take an event but not input it into the function, otherwise the function will need an event argument, which caused problems when the user clicks the button instead of using the binding since clicking doesn't send the event. - Fixed a bug that caused the input barcode, name, and asset to be overwritten when only one of the input fields was shown. This is because the other hidden input fields would be saved as "". - Added a barcode only mode for the view in which only the barcode entry is show. - Added a asset only mode for the view in which only the asset entry is show. - Added a serial only mode for the view in which only the serial entry is show. This isn't be used at the moment.

--- entry_controller.py
+++ entry_controller.py
@@ -10,15 +10,14 @@ filename = os.path.splitext(filename)[0]
 logger = loggers.FileLogger(name=filename, level=loggers.DEBUG)
 
 
-class EntryViewController(object):
+class EntryController(object):
     def __init__(self, master, computer, dt):
-        #self.model = model
         self.entry_view = EntryView(master)
         self.entry_view.protocol('WM_DELETE_WINDOW', self.close_button_clicked)
         self.computer = computer
         self.dt = dt
 
-        self.populate_entry_fields()
+        # self.populate_entry_fields()
 
         self.set_to_middle(self.entry_view)
 
@@ -27,7 +26,7 @@ class EntryViewController(object):
         self.entry_view.grab_set()
 
         self.entry_view.submit_btn.config(command=self.submit_btn_clicked)
-        self.entry_view.bind('<Return>', self.submit_btn_clicked)
+        self.entry_view.bind('<Return>', lambda event: self.submit_btn_clicked())
 
 
     def set_to_middle(self, window):
@@ -42,18 +41,21 @@ class EntryViewController(object):
         window.geometry('+%d+%d' % (screen_width / 2 - window.winfo_width() / 2, screen_height / 4))
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-    def submit_btn_clicked(self, sender):
+    def submit_btn_clicked(self):
         self.store_entry_fields()
         self.dt.proceed = True
         self.entry_view.destroy()
 
     def store_entry_fields(self):
-        self.computer.barcode = self.entry_view.barcode_entry.get()
-        self.computer.asset = self.entry_view.asset_entry.get()
-        self.computer.name = self.entry_view.name_entry.get()
+        if self.entry_view.barcode_entry.get() != "":
+            self.computer.barcode = self.entry_view.barcode_entry.get()
+        if self.entry_view.asset_entry.get() != "":
+            self.computer.asset = self.entry_view.asset_entry.get()
+        if self.entry_view.name_entry.get() != "":
+            self.computer.name = self.entry_view.name_entry.get()
 
     def populate_entry_fields(self):
-        noneFilter = lambda x : "" if x == None else x
+        noneFilter = lambda x : "" if x is None else x
         self.entry_view.barcode_entry.insert(0, "{}".format(noneFilter(self.computer.barcode)))
         self.entry_view.asset_entry.insert(0, "{}".format(noneFilter(self.computer.asset)))
         self.entry_view.name_entry.insert(0, "{}".format(noneFilter(self.computer.name)))
@@ -62,3 +64,26 @@ class EntryViewController(object):
         self.dt.proceed = False
         self.entry_view.destroy()
 
+    def barcode_only(self):
+        self.entry_view.title("Search JSS")
+        self.entry_view.text_lbl.config(text="Enter the barcode.")
+        self.entry_view.name_entry.grid_forget()
+        self.entry_view.name_lbl.grid_forget()
+        self.entry_view.asset_entry.grid_forget()
+        self.entry_view.asset_lbl.grid_forget()
+
+    def asset_only(self):
+        self.entry_view.title("Search JSS")
+        self.entry_view.text_lbl.config(text="Enter the asset.")
+        self.entry_view.name_entry.grid_forget()
+        self.entry_view.name_lbl.grid_forget()
+        self.entry_view.barcode_entry.grid_forget()
+        self.entry_view.barcode_lbl.grid_forget()
+
+    def serial_only(self):
+        self.entry_view.title("Search JSS")
+        self.entry_view.text_lbl.config(text="Enter the asset.")
+        self.entry_view.name_entry.grid_forget()
+        self.entry_view.name_lbl.grid_forget()
+        self.entry_view.barcode_entry.grid_forget()
+        self.entry_view.barcode_lbl.grid_forget()
