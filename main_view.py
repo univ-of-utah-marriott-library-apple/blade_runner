1e5f2779bc04a4beedaef57160e991bb848c02af
Thackery Archuletta <t.archuletta@utah.edu>
2018-12-17 16:35:00 -0700

    - Passing the controller to the view to prevent circular dependencies. - Added a salvage and storage button and added functions for these buttons. - Moved function assignments for the view from the controller back into the view.

--- main_view.py
+++ main_view.py
@@ -1,27 +1,59 @@
 import Tkinter as tk
 
 
-class View(tk.Toplevel):
-
-    def __init__(self, master):
+class MainView(tk.Toplevel):
+    def __init__(self, master, controller):
         tk.Toplevel.__init__(self, master)
+        # self.master = master
         self.title("Blade Runner")
-        self.protocol('WM_DELETE_WINDOW', self.master.destroy)
+        self.protocol('WM_DELETE_WINDOW', self.exit_btn_clicked)
+
+        # Set the controller
+        self.controller = controller
 
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Creating components
         self.header = tk.Label(self, text="Blade-Runner", font=("Copperplate", 40))
         self.header.grid(row=0)
 
-        self.choose_lbl = tk.Label(self, text="Choose one of the following options:", )
-        self.choose_lbl.grid(row=1)
+        self.selection_lbl = tk.Label(self)
 
-        self.serial_btn = tk.Button(self, text="Get Serial Number", fg='blue')
-        self.serial_btn.grid(row=2, column=0, sticky='EW')
+        self.choose_lbl = tk.Label(self, text="What is the inventory status of this computer?")
+        self.choose_lbl.grid(row=2)
 
-        self.barcode_btn = tk.Button(self, text="Enter Barcode Number")
-        self.barcode_btn.grid(row=3, column=0, sticky='EW')
+        self.salvage_btn = tk.Button(self, text="Salvage", fg='blue', command=lambda: self.salvage_btn_clicked())
+        self.salvage_btn.grid(row=3, column=0, sticky='EW')
 
-        self.asset_btn = tk.Button(self, text="Enter Asset Number")
-        self.asset_btn.grid(row=4, column=0, sticky='EW')
+        self.storage_btn = tk.Button(self, text="Storage", command=lambda: self.storage_btn_clicked())
+        self.storage_btn.grid(row=4, column=0, sticky='EW')
+
+        self.serial_btn = tk.Button(self, text="Get Serial Number", fg='blue', command=lambda: self.serial_btn_clicked())
+        self.barcode_btn = tk.Button(self, text="Enter Barcode Number", command=lambda: self.barcode_btn_clicked())
+        self.asset_btn = tk.Button(self, text="Enter Asset Number", command=lambda: self.asset_btn_clicked())
+
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # Bind the return key to the serial button
+        self.bind('<Return>', self.serial_btn_clicked)
+
+        # Set the window to the middle of the screen
+        self.controller.set_to_middle(self)
+
+    def serial_btn_clicked(self):
+        self.controller.serial_input()
+
+    def barcode_btn_clicked(self):
+        self.controller.barcode_input()
+
+    def asset_btn_clicked(self):
+        self.controller.asset_input()
+
+    def salvage_btn_clicked(self):
+        self.controller.options_scene(self.salvage_btn)
+
+    def storage_btn_clicked(self):
+        self.controller.options_scene(self.storage_btn)
+
+    def exit_btn_clicked(self):
+        self.master.destroy()
+
+
