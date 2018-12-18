43450008acb94c534c9f2bfc740773c54529b838
Thackery Archuletta <t.archuletta@utah.edu>
2018-12-18 16:22:59 -0700

    - Adding slackify_reminder_daemon. - Sends notifactions to a slack channel according to the time period specified. Only notifies slack channel during certain hours of the day. Currently that is between 9 am and 6 pm.

---
+++ slackify_reminder_daemon.py
@@ -0,0 +1,32 @@
+from daemon import daemon
+import time
+import subprocess
+import os
+import sys
+import plistlib
+from management_tools import filelock
+
+def slackify_reminder():
+    python_bin = '/usr/bin/python'
+    script_path = os.path.join(script_dir, 'slackify.py')
+    channel = slack_data['slack_channel']
+    message = 'Reminder: Blade-Runner finished.'
+    while True:
+        hour = time.localtime().tm_hour
+        if 9 <= hour < 18:
+            cmd = [python_bin, script_path, channel, message]
+            subprocess.Popen(cmd, stderr=subprocess.STDOUT)
+        time.sleep(2*3600)
+
+
+def run_daemon():
+    print("Running Slackify daemon.")
+    with daemon.DaemonContext(stdout=sys.stdout, stderr=sys.stdout, pidfile=filelock.FileLock('/tmp/spam.pid', timeout=5)):
+        slackify_reminder()
+
+
+if __name__ == "__main__":
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    slack_plist = os.path.join(script_dir, "private/slack_config.plist")
+    slack_data = plistlib.readPlist(slack_plist)
+    run_daemon()
