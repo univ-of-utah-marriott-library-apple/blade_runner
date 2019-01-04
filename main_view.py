94caaa6daf20fa2395daaaed613f0fda3f1f5ca1
Thackery Archuletta <t.archuletta@utah.edu>
2019-01-04 14:35:01 -0700

    - Removed Salvage and Storage buttons. Replaced them with a file drop down combobox that lets the user select an offboard xml config. This allows Blade-Runner to be customizable for others.

--- main_view.py
+++ main_view.py
@@ -1,5 +1,5 @@
 import Tkinter as tk
-
+import ttk
 
 class MainView(tk.Toplevel):
     def __init__(self, master, controller):
@@ -18,18 +18,28 @@ class MainView(tk.Toplevel):
 
         self.selection_lbl = tk.Label(self)
 
-        self.choose_lbl = tk.Label(self, text="What is the inventory status of this computer?")
+        self.choose_lbl = tk.Label(self, text="Select an offboard configuration file below.")
         self.choose_lbl.grid(row=2)
 
-        self.salvage_btn = tk.Button(self, text="Salvage", fg='blue', command=lambda: self.salvage_btn_clicked())
-        self.salvage_btn.grid(row=3, column=0, sticky='EW')
+        self.combobox = ttk.Combobox(self)
+        self.combobox.grid(row=3, column=0, stick='EW')
 
-        self.storage_btn = tk.Button(self, text="Storage", command=lambda: self.storage_btn_clicked())
-        self.storage_btn.grid(row=4, column=0, sticky='EW')
+        self.next_btn = tk.Button(self, text="Next", fg='blue', command=lambda: self.next_btn_clicked())
+        self.next_btn.grid(row=4, column=0, sticky='EW')
+
+        # self.choose_lbl = tk.Label(self, text="What is the inventory status of this computer?")
+        # self.choose_lbl.grid(row=2)
+        #
+        # self.salvage_btn = tk.Button(self, text="Salvage", fg='blue', command=lambda: self.salvage_btn_clicked())
+        # self.salvage_btn.grid(row=3, column=0, sticky='EW')
+        #
+        # self.storage_btn = tk.Button(self, text="Storage", command=lambda: self.storage_btn_clicked())
+        # self.storage_btn.grid(row=4, column=0, sticky='EW')
 
         self.serial_btn = tk.Button(self, text="Get Serial Number", fg='blue', command=lambda: self.serial_btn_clicked())
         self.barcode_btn = tk.Button(self, text="Enter Barcode Number", command=lambda: self.barcode_btn_clicked())
         self.asset_btn = tk.Button(self, text="Enter Asset Number", command=lambda: self.asset_btn_clicked())
+
 
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Bind the return key to the serial button
@@ -37,6 +47,9 @@ class MainView(tk.Toplevel):
 
         # Set the window to the middle of the screen
         self.controller.set_to_middle(self)
+
+    def next_btn_clicked(self):
+        self.controller.options_scene(self.combobox.get())
 
     def serial_btn_clicked(self):
         self.controller.serial_input()
