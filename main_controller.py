8e1b2e5f6edfabc5aa9efb2bea89b67cd0000dc4
Thackery Archuletta <t.archuletta@utah.edu>
2019-01-04 14:27:09 -0700

    - Added offboard config and slack config to __init__. Helps with unit testing. - Added populate_config_combox(). Populates the combobox with available xml files in a given directory - Changed barcode to barcode_1. - Changed asset to asset_tag - Added barcode_2. - Changed serial to serial_number. - Commented out verification code for checking whether the fields were successfully sent to the JSS. I talked to Todd about it and he said that it's not needed since JAMF will send a return code that tells you if the data was successfully submitted. - Added send_slack_message() - Added start_slackify_reminder_daemon - Added terminate() to terminate the application.

--- main_controller.py
+++ main_controller.py
@@ -16,9 +16,10 @@ from management_tools.slack import IncomingWebhooksSender as IWS
 from jss_doc import JssDoc
 from tuggyboat import TuggyBoat
 
+# TODO Interface with a Trello board and dynamically create lists for the DEP
 
 class MainController(object):
-    def __init__(self, root, server):
+    def __init__(self, root, server, slack_data):
 
         self.model = Model(server)
         self.jss_server = server
@@ -36,24 +37,41 @@ class MainController(object):
         self.barcode_searched = False
         self.asset_searched = False
         self.serial_searched = False
-        self.print_enabled = True
+        self.print_enabled = False
         self.slack_enabled = True
+
+        self.offboard_config = None
+        self.slack_data = slack_data
+        self.private_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "private")
 
     def run(self):
         self.main_view = MainView(self.root, self)
+        self.populate_config_combobox()
         self.refocus()
         self.main_view.mainloop()
 
-    def options_scene(self, sender):
+    def populate_config_combobox(self):
+        private_files = os.listdir(self.private_dir)
+        offboard_configs = []
+        for file in private_files:
+            if file.endswith(".xml"):
+                offboard_configs.append(file)
+        self.main_view.combobox.config(values=offboard_configs)
+        self.main_view.combobox.current(0)
+
+    def options_scene(self, config):
+        self.offboard_config = config
         self.forget_buttons()
         self.main_view.selection_lbl.grid(row=1)
 
-        if sender is self.main_view.salvage_btn:
-            self.main_view.selection_lbl.config(text="Inventory Status: SALVAGE")
-            self.inventory_status = "Salvage"
-        elif sender is self.main_view.storage_btn:
-            self.main_view.selection_lbl.config(text="Inventory Status: STORAGE")
-            self.inventory_status = "Storage"
+        self.main_view.selection_lbl.config(text="Current configuration file: " + config)
+
+        # if sender is self.main_view.salvage_btn:
+        #     self.main_view.selection_lbl.config(text="Inventory Status: SALVAGE")
+        #     self.inventory_status = "Salvage"
+        # elif sender is self.main_view.storage_btn:
+        #     self.main_view.selection_lbl.config(text="Inventory Status: STORAGE")
+        #     self.inventory_status = "Storage"
 
         self.main_view.choose_lbl.config(text="Choose one of the following options:")
 
@@ -64,13 +82,15 @@ class MainController(object):
         self.main_view.asset_btn.grid(row=4, column=0, sticky='EW')
 
     def forget_buttons(self):
-        self.main_view.salvage_btn.grid_forget()
-        self.main_view.storage_btn.grid_forget()
+        self.main_view.next_btn.grid_forget()
+
+        # self.main_view.salvage_btn.grid_forget()
+        # self.main_view.storage_btn.grid_forget()
 
     def serial_input(self):
-        self.computer.serial = self.computer.get_serial()
-        logger.debug("Serial number: {0!r}".format(self.computer.serial))
-        self.jss_server.search_param = self.computer.serial
+        self.computer.serial_number = self.computer.get_serial()
+        logger.debug("Serial number: {0!r}".format(self.computer.serial_number))
+        self.jss_server.search_param = self.computer.serial_number
         self.dt.proceed = True
         self.computer.jss_id = self.jss_server.match(self.jss_server.search_param)
         self.serial_searched = True
@@ -81,8 +101,8 @@ class MainController(object):
         self.entry_controller.barcode_only()
         # Wait
         self.main_view.wait_window(window=self.entry_controller.entry_view)
-        print(self.computer.barcode)
-        self.jss_server.search_param = self.computer.barcode
+        print(self.computer.barcode_1)
+        self.jss_server.search_param = self.computer.barcode_1
         self.computer.jss_id = self.jss_server.match(self.jss_server.search_param)
         self.barcode_searched = True
         self.dt.proceed = True
@@ -93,7 +113,7 @@ class MainController(object):
         self.entry_controller.asset_only()
         # Wait
         self.main_view.wait_window(window=self.entry_controller.entry_view)
-        self.jss_server.search_param = self.computer.asset
+        self.jss_server.search_param = self.computer.asset_tag
         self.computer.jss_id = self.jss_server.match(self.jss_server.search_param)
         self.asset_searched = True
         self.dt.proceed = True
@@ -129,8 +149,8 @@ class MainController(object):
             self.main_view.wait_window(window=self.entry_controller.entry_view)
 
             # Make sure the serial field for the computer object was updated/set.
-            if self.computer.serial is None:
-                self.computer.serial = self.computer.get_serial()
+            if self.computer.serial_number is None:
+                self.computer.serial_number = self.computer.get_serial()
 
             # If user closes out of entry view window using the x button, proceed = False, and the function exits.
             if self.dt.proceed is False:
@@ -154,25 +174,38 @@ class MainController(object):
 
         # If JSS ID exists
         elif self.computer.jss_id is not None:
-            # Get tugboat fields before altering them.
-            tugboat_fields = self.tuggyboat.get_tugboat_fields(self.computer.jss_id)
-            logger.debug("Original fields before alteration: {}".format(pformat(tugboat_fields)))
+            # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+            # CHANGED
+
+            # # Get tugboat fields before altering them.
+            # tugboat_fields = self.tuggyboat.get_tugboat_fields(self.computer.jss_id)
+            # logger.debug("Original fields before alteration: {}".format(pformat(tugboat_fields)))
+
+            # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+            # CHANGED
+            prev_barcode_1 = self.jss_server.get_barcode_1(self.computer.jss_id)
+            prev_barcode_2 = self.jss_server.get_barcode_2(self.computer.jss_id)
+            prev_asset = self.jss_server.get_asset_tag(self.computer.jss_id)
+            prev_name = self.jss_server.get_computer_name(self.computer.jss_id)
 
             # Store the unaltered barcode, asset, and name fields.
-            prev_barcode = tugboat_fields['general']['barcode_1']
-            prev_asset = tugboat_fields['general']['asset_tag']
-            prev_name = self.jss_server.get_prev_name(self.computer.jss_id)
+            # prev_barcode_1 = tugboat_fields['general']['barcode_1']
+            # prev_asset = tugboat_fields['general']['asset_tag']
+            # prev_name = self.jss_server.get_prev_name(self.computer.jss_id)
+
+            # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
             # If neither the barcode or asset have been entered, set the fields to the returned JSS fields
             if not self.barcode_searched and not self.asset_searched:
-                self.computer.barcode = prev_barcode
-                self.computer.asset = prev_asset
+                self.computer.barcode_1 = prev_barcode_1
+                self.computer.barcode_2 = prev_barcode_2
+                self.computer.asset_tag = prev_asset
                 self.computer.name = prev_name
 
-            if self.computer.serial is None:
-                self.computer.serial = self.computer.get_serial()
+            if self.computer.serial_number is None:
+                self.computer.serial_number = self.computer.get_serial()
 
-            logger.debug("Previous barcode: {}".format(prev_barcode))
+            logger.debug("Previous barcode: {}".format(prev_barcode_1))
             logger.debug("Previous asset: {}".format(prev_asset))
             logger.debug("Previous name: {}".format(prev_name))
 
@@ -190,32 +223,32 @@ class MainController(object):
                 return
 
             # Check to see what fields changed after the user updated the fields through the entry view window.
-            if self.computer.barcode == prev_barcode:
-                logger.debug("Barcode {} is correct.".format(self.computer.barcode))
-                self.computer.barcode = None
+            if self.computer.barcode_1 == prev_barcode_1:
+                logger.debug("Barcode {} is correct.".format(self.computer.barcode_1))
+                self.computer.barcode_1 = None
             else:
-                self.computer.incorrect_barcode = prev_barcode
+                self.computer.incorrect_barcode = prev_barcode_1
                 logger.debug("Barcode {} is incorrect.".format(self.computer.incorrect_barcode))
 
-            if self.computer.asset == prev_asset:
-                logger.debug("Asset {} is correct.".format(self.computer.asset))
-                self.computer.asset = None
+            if self.computer.asset_tag == prev_asset:
+                logger.debug("Asset {} is correct.".format(self.computer.asset_tag))
+                self.computer.asset_tag = None
             else:
                 self.computer.incorrect_asset = prev_asset
                 logger.debug("Asset {} is incorrect.".format(self.computer.incorrect_asset))
 
             if self.jss_server.get_serial(self.computer.jss_id) == self.computer.get_serial():
-                logger.debug("Serial {} is correct.".format(self.computer.serial))
-                self.computer.serial = None
+                logger.debug("Serial {} is correct.".format(self.computer.serial_number))
+                self.computer.serial_number = None
             else:
-                self.computer.serial = self.computer.get_serial()
-                self.computer.incorrect_serial = tugboat_fields['general']['serial_number']
+                self.computer.serial_number = self.computer.get_serial()
+                self.computer.incorrect_serial = self.jss_server.get_serial(self.computer.jss_id)
                 logger.debug("JSS serial {} is incorrect.".format(self.computer.incorrect_serial))
 
             if self.computer.name == prev_name:
                 self.computer.name = None
 
-            # Check if ManagedA
+            # Check if Managed
             managed = self.jss_server.get_managed_status(self.computer.jss_id)
             logger.debug("Management status is {}".format(managed))
 
@@ -227,76 +260,102 @@ class MainController(object):
             # Push enroll fields to JSS server
             self.jss_server.push_enroll_fields(self.computer)
 
-        # Stores offboarded values for later comparison
-        fields_to_offboard = self.tuggyboat.get_offboard_fields(self.computer.jss_id)
-        logger.debug("Fields to be offboarded: {}".format(pformat(fields_to_offboard)))
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # CHANGED
+        # Get xml fields before altering them.
+        # pre_offboard_fields = self.jss_server.get_xml_fields(self.computer.jss_id, offboard_xml)
+        # logger.debug("Fields before offboarding: {}".format(pformat(pre_offboard_fields)))
 
-        offboarded_values = self.tuggyboat.set_offboard_fields(fields_to_offboard, inventory_status=self.inventory_status)
-        logger.debug("Offboarded fields to be sent: {}".format(pformat(offboarded_values)))
+        # Stores offboarded values for later comparison
+        # fields_to_offboard = self.tuggyboat.get_offboard_fields(self.computer.jss_id)
+        # logger.debug("Fields to be offboarded: {}".format(pformat(fields_to_offboard)))
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # CHANGED
+
+
+        # offboarded_values = self.tuggyboat.set_offboard_fields(fields_to_offboard, inventory_status=self.inventory_status)
+        # logger.debug("Offboarded fields to be sent: {}".format(pformat(offboarded_values)))
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # CHANGED
+        self.jss_server.push_xml_fields(os.path.join(self.private_dir, self.offboard_config), self.computer.jss_id)
 
         # Push offboard fields to the JSS
-        self.tuggyboat.push_offboard_fields(self.computer.jss_id, offboarded_values)
+        # self.tuggyboat.push_offboard_fields(self.computer.jss_id, offboarded_values)
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # CHANGED
+        # post_offboard_fields = self.jss_server.get_xml_fields(self.computer.jss_id, offboard_xml)
+        # logger.debug("Fields after offboarding: {}".format(pformat(post_offboard_fields)))
 
         # Retrieves new computer data after sending the offboard fields for later comparison
-        new_jss_data = self.tuggyboat1.get_offboard_fields(self.computer.jss_id)
-        logger.debug("New retrieved fields: {}".format(pformat(new_jss_data)))
+        # new_jss_data = self.tuggyboat1.get_offboard_fields(self.computer.jss_id)
+        # logger.debug("New retrieved fields: {}".format(pformat(new_jss_data)))
 
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Compare new JSS data against the sent offboarded fields. They should be the same.
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # CHANGED
         # Get the new managed status and make sure it's false
-        new_managed_status = new_jss_data['general']['remote_management']['managed'].lower()
-        logger.debug("new_managed_status: {}".format(new_managed_status))
+        post_managed_status = self.jss_server.get_managed_status(self.computer.jss_id)
+        logger.debug("post_managed_status: {}".format(post_managed_status))
 
-        if new_managed_status != 'false':
+        if post_managed_status != 'false':
             raise SystemExit("ERROR: Managed status is not false after offboarding.")
 
-        # Get the new computer name and make sure it's the same as the offboarded name
-        new_computer_name = new_jss_data['general']['computer_name']
-        logger.debug("new_computer_name: {}".format(new_computer_name))
+        # # Get the new computer name and make sure it's the same as the offboarded name
+        # new_computer_name = new_jss_data['general']['computer_name']
+        # logger.debug("new_computer_name: {}".format(new_computer_name))
+        #
+        # intended_computer_name = offboarded_values['general']['computer_name']
+        # logger.debug("intented_computer_name: {}".format(intended_computer_name))
+        #
+        # if new_computer_name != intended_computer_name:
+        #     raise SystemExit(
+        #         "ERROR: Computer name is " + new_computer_name + " but should be " + intended_computer_name)
 
-        intended_computer_name = offboarded_values['general']['computer_name']
-        logger.debug("intented_computer_name: {}".format(intended_computer_name))
-
-        if new_computer_name != intended_computer_name:
-            raise SystemExit(
-                "ERROR: Computer name is " + new_computer_name + " but should be " + intended_computer_name)
-
-        # Remove managed status from new_JSS_data and offboarded_values to compare the rest of the data.
-        new_jss_data['general']['remote_management'].pop('managed', None)
-        offboarded_values['general']['remote_management'].pop('managed', None)
-
-        # Check new data against offboarded fields
-        if new_jss_data != offboarded_values:
-            raise SystemExit("ERROR: Some of the new fields do not match offboard values")
-        logger.debug("All offboarded values were successfully received by the JSS.")
+        # # Remove managed status from new_JSS_data and offboarded_values to compare the rest of the data.
+        # new_jss_data['general']['remote_management'].pop('managed', None)
+        # offboarded_values['general']['remote_management'].pop('managed', None)
+        #
+        # # Check new data against offboarded fields
+        # if new_jss_data != offboarded_values:
+        #     raise SystemExit("ERROR: Some of the new fields do not match offboard values")
+        # logger.debug("All offboarded values were successfully received by the JSS.")
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Notify slack channel that offboard tool has finished.
-        if slack_data['slack_enabled'] == "True":
-            bot.send_message("Offboarding finished.")
+        if self.slack_data['slack_enabled'] == "True":
+            self.send_slack_message("Offboarding complete.")
 
-        logger.info("Building HTML Page")
+        # Prepare document for printing
         doc = JssDoc(self.jss_server, self.computer.jss_id, self.computer)
         doc.create_html()
         doc.html2pdf()
         if self.print_enabled:
             doc.applescript_print()
-        logger.info("Building HTML Page Finished")
 
-        final_tugboat_fields = self.jss_server.get_tugboat_fields(self.computer.jss_id)
-        logger.debug("Final Tugboat fields: \n{}".format(pformat(final_tugboat_fields)))
+        # final_tugboat_fields = self.jss_server.get_tugboat_fields(self.computer.jss_id)
+        # logger.debug("Final Tugboat fields: \n{}".format(pformat(final_tugboat_fields)))
 
+        if self.slack_data['slackify_daemon_enabled'] == 'True':
+            self.start_slackify_reminder_dameon()
+
+        logger.info("Blade Runner successfully finished.")
+        self.terminate()
+
+    def send_slack_message(self, message):
+        current_ip = socket.gethostbyname(socket.gethostname())
+        slack_bot = IWS(self.slack_data['slack_url'], bot_name=current_ip, channel=self.slack_data['slack_channel'])
+        slack_bot.send_message(message)
+
+    def start_slackify_reminder_dameon(self):
         # Start Slackify reminder daemon
-        if slack_data['slackify_daemon_enabled'] == 'True':
             script_dir = os.path.dirname(os.path.abspath(__file__))
             cmd = ['/usr/bin/python', os.path.join(script_dir, 'slackify_reminder_daemon.py')]
             subprocess.Popen(cmd, stderr=subprocess.STDOUT)
 
+    def terminate(self):
         # Destroy the main view and the root mainloop.
         self.main_view.destroy()
         self.root.destroy()
-
-        logger.info("Blade Runner successfully finished.")
 
     def refocus(self):
         # Python tkinter window gets selected automatically
@@ -333,18 +392,24 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.withdraw()
 
+    # Read from jss config plist and set up the JSS server
     blade_runner_dir = os.path.dirname(abs_file_path)
     jss_server_plist = os.path.join(blade_runner_dir, "private/jss_server_config.plist")
     jss_server_data = plistlib.readPlist(jss_server_plist)
     jss_server = JssServer(**jss_server_data)
     logger.debug(jss_server.jss_url)
 
+    # Read from Slack config plist to set up Slack notifications
     slack_plist = os.path.join(blade_runner_dir, "private/slack_config.plist")
     slack_data = plistlib.readPlist(slack_plist)
     current_ip = socket.gethostbyname(socket.gethostname())
     bot = IWS(slack_data['slack_url'], bot_name=current_ip, channel=slack_data['slack_channel'])
 
-    app = MainController(root, jss_server)
+    # Read from the offboard config plist to set up which fields will be offboarded
+    offboard_xml = "private/offboard_config.xml"
+
+    # Run the application
+    app = MainController(root, jss_server, slack_data)
     app.run()
 
 
