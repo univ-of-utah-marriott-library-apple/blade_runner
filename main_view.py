1e6317d7a3c5ccfeff5316843e78d55d5b8046e1
Thackery Archuletta <t.archuletta@utah.edu>
2019-01-15 16:48:04 -0700

    Removed code for Salvage and Storage buttons.
    Added barcode_2 button.
    
    Fixed Return event bug. When return was pressed an event argument would
    be sent to the callback function but the callback function didn't take
    inputs. Fixed this by adding "lambda: event" so that the lambda
    expression would take the event input and not input it into the
    callback.
    
    Added a Map event for the window. This allows the combobox to be
    populated after the window has finished loading. A Map event calls the
    callback once the window it's bound to has finished loading.
    
    Added a single function that determines the input type of the button,
    e.g., barcode 1, barcode 2, asset tag, or serial number.
    
    Added options_scene().
    
    Added populate_combobox() which calls the controller's
    populate_combobox().

--- main_view.py
+++ main_view.py
@@ -27,46 +27,72 @@ class MainView(tk.Toplevel):
         self.next_btn = tk.Button(self, text="Next", fg='blue', command=lambda: self.next_btn_clicked())
         self.next_btn.grid(row=4, column=0, sticky='EW')
 
-        # self.choose_lbl = tk.Label(self, text="What is the inventory status of this computer?")
-        # self.choose_lbl.grid(row=2)
-        #
-        # self.salvage_btn = tk.Button(self, text="Salvage", fg='blue', command=lambda: self.salvage_btn_clicked())
-        # self.salvage_btn.grid(row=3, column=0, sticky='EW')
-        #
-        # self.storage_btn = tk.Button(self, text="Storage", command=lambda: self.storage_btn_clicked())
-        # self.storage_btn.grid(row=4, column=0, sticky='EW')
+        self.serial_btn = tk.Button(self, text="Get Serial Number", fg='blue', command=lambda: self.input_btn_clicked('serial_number'))
+        # self.barcode_btn = tk.Button(self, text="Enter Barcode Number", command=lambda: self.barcode_btn_clicked())
+        # self.asset_btn = tk.Button(self, text="Enter Asset Number", command=lambda: self.asset_btn_clicked())
 
-        self.serial_btn = tk.Button(self, text="Get Serial Number", fg='blue', command=lambda: self.serial_btn_clicked())
-        self.barcode_btn = tk.Button(self, text="Enter Barcode Number", command=lambda: self.barcode_btn_clicked())
-        self.asset_btn = tk.Button(self, text="Enter Asset Number", command=lambda: self.asset_btn_clicked())
-
+        self.barcode_1_btn = tk.Button(self, text="Enter Barcode 1", command=lambda: self.input_btn_clicked('barcode_1'))
+        self.barcode_2_btn = tk.Button(self, text="Enter Barcode 2", command=lambda: self.input_btn_clicked('barcode_2'))
+        self.asset_btn = tk.Button(self, text="Enter Asset Number", command=lambda: self.input_btn_clicked('asset_tag'))
 
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Bind the return key to the serial button
-        self.bind('<Return>', self.serial_btn_clicked)
-
+        self.bind('<Return>', lambda event: self.next_btn_clicked())
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # After the window loads, the function is fired. This is the Map event, or when the window has mapped.
+        self.bind('<Map>', lambda event: self.populate_combobox())
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Set the window to the middle of the screen
         self.controller.set_to_middle(self)
 
+    def input_btn_clicked(self, input_type):
+        if input_type == 'barcode_1':
+            self.controller.determine_input_type(input_type)
+        if input_type == 'barcode_2':
+            self.controller.determine_input_type(input_type)
+        if input_type == 'asset_tag':
+            self.controller.determine_input_type(input_type)
+        if input_type == 'serial_number':
+            self.controller.determine_input_type(input_type)
+
     def next_btn_clicked(self):
-        self.controller.options_scene(self.combobox.get())
+        self.options_scene()
+        self.bind('<Return>', lambda event: self.serial_btn_clicked())
+        self.controller.save_offboard_config(self.combobox.get())
 
     def serial_btn_clicked(self):
         self.controller.serial_input()
 
     def barcode_btn_clicked(self):
-        self.controller.barcode_input()
+        self.controller.barcode_1_input()
 
     def asset_btn_clicked(self):
         self.controller.asset_input()
 
-    def salvage_btn_clicked(self):
-        self.controller.options_scene(self.salvage_btn)
-
-    def storage_btn_clicked(self):
-        self.controller.options_scene(self.storage_btn)
-
     def exit_btn_clicked(self):
         self.master.destroy()
+
+    def populate_combobox(self):
+        self.controller.populate_config_combobox()
+
+    def options_scene(self):
+        self.next_btn.grid_forget()
+
+        self.selection_lbl.grid(row=1)
+        self.selection_lbl.config(text="Current configuration file: " + self.combobox.get())
+
+        self.choose_lbl.config(text="Choose one of the following options:")
+
+        self.serial_btn.grid(row=2, column=0, sticky='EW')
+
+        if self.controller.verify_data['barcode_1'] is True:
+            self.barcode_1_btn.grid(row=3, column=0, sticky='EW')
+
+        if self.controller.verify_data['barcode_2'] is True:
+            self.barcode_2_btn.grid(row=4, column=0, sticky='EW')
+
+        if self.controller.verify_data['asset_tag'] is True:
+            self.asset_btn.grid(row=5, column=0, sticky='EW')
+
 
 
