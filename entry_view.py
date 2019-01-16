1ffe7137a293c3a4103d2b81c3e25d6c37f3ff76
Thackery Archuletta <t.archuletta@utah.edu>
2019-01-15 17:43:57 -0700

    Added barcode 1 and barcode 2 vars.
    Removed grid calls and placed them in the entry controller.

--- entry_view.py
+++ entry_view.py
@@ -2,7 +2,6 @@ import Tkinter as tk
 
 
 class EntryView(tk.Toplevel):
-
     def __init__(self, master):
         tk.Toplevel.__init__(self, master)
         self.title("Create New JSS Record")
@@ -18,28 +17,30 @@ class EntryView(tk.Toplevel):
                                             "following fields:")
         self.text_lbl.grid(row=0)
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
-        # Barcode components
-        self.barcode_lbl = tk.Label(content_frame, text='Barcode:')
-        self.barcode_lbl.grid(row=1, column=0, sticky="E")
-        self.barcode_entry = tk.Entry(content_frame)
-        self.barcode_entry.grid(row=1, column=1)
+        # Barcode_1 components
+        self.barcode_1_lbl = tk.Label(content_frame, text='Barcode 1:')
+        self.barcode_1_entry = tk.Entry(content_frame)
+        # self.barcode_1_entry.grid(row=1, column=1)
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # Barcode_2 components
+        self.barcode_2_lbl = tk.Label(content_frame, text='Barcode 2:')
+        self.barcode_2_entry = tk.Entry(content_frame)
+        # self.barcode_2_entry.grid(row=2, column=1)
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Asset components
-        self.asset_lbl = tk.Label(content_frame, text='Asset:')
-        self.asset_lbl.grid(row=2, column=0, sticky="E")
+        self.asset_lbl = tk.Label(content_frame, text='Asset Tag:')
         self.asset_entry = self.asset_entry = tk.Entry(content_frame)
-        self.asset_entry.grid(row=2, column=1)
+        # self.asset_entry.grid(row=3, column=1)
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Name components
         self.name_lbl = tk.Label(content_frame, text='Name:')
-        self.name_lbl.grid(row=3, column=0, sticky="E")
         self.name_entry = tk.Entry(content_frame)
-        self.name_entry.grid(row=3, column=1)
+        # self.name_entry.grid(row=4, column=1)
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Submit button
         self.submit_btn = tk.Button(content_frame, text='Submit')
-        self.submit_btn.grid(row=4, column=1)
+        self.submit_btn.grid(row=5, column=1)
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
-
         header_frame.grid(row=0)
         content_frame.grid(row=1)
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>^
