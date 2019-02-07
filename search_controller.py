14635c295ba9ac0e976a0023fcc6f5fe2459fd0c
Thackery Archuletta <t.archuletta@utah.edu>
2019-02-07 16:54:37 -0700

    Updated.

--- search_controller.py
+++ search_controller.py
@@ -2,19 +2,25 @@ from entry_controller_base import EntryController
 import inspect
 from management_tools import loggers
 import os
+from entry_view import EntryView
 
 
 class SearchController(EntryController):
     def __init__(self, master, computer, input_type):
-        super(SearchController, self).__init__(master, computer)
+        view = EntryView(master, self)
+        super(SearchController, self).__init__(master, computer, view)
+        self.set_to_middle(view)
         self.proceed = None
         self.input_type = input_type
-        self.determine_widget(input_type)
+        self.determine_user_widget(input_type)
+        self.populate_user_entry(input_type)
+        self.focus_entry(input_type)
+        self.set_to_middle(view)
 
     def proceed_operation(self):
-        self.store_entry_field(self.input_type)
-        self.proceed = True
-        self.entry_view.destroy()
+        if self.store_entry_field(self.input_type):
+            self.proceed = True
+            self.entry_view.destroy()
 
     def cancel_operation(self):
         self.proceed = False
