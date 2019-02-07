f98fad27b206644708f824d145dfbb1c29d23920
Thackery Archuletta <t.archuletta@utah.edu>
2019-02-07 16:50:51 -0700

    Updated append_xml_name().
    Added append_xml_str_name(). This was added to fix a bug in
    main_controller in which the config file itself was being updated.
    Now the config is read, saved as a string, and modifications are made
    to the string instead of the file.
    
    Added append_additional_note(). Appends to additional item note
    extension attribute.
    
    Added timestamp_note(). Calls append_additional_note().
    
    Added set_additional_note() for overwriting additional note.
    
    Added xml_to_string(). Turns an xml into a string.

--- user_xml_updater.py
+++ user_xml_updater.py
@@ -1,15 +1,65 @@
 import xml.etree.cElementTree as ET
+import datetime
 
 
 def append_xml_name(string, xml_file):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
+    xml_tree = ET.parse(xml_file)
+    xml_root = xml_tree.getroot()
 
-    for general_element in root.findall("./general"):
+    for general_element in xml_root.findall("./general"):
         name_element = general_element.find('name')
         name_element.text = name_element.text + string
 
-    tree.write(xml_file)
+    return ET.tostring(xml_root)
+
+
+def append_xml_str_name(string, xml_str):
+    xml_root = ET.fromstring(xml_str)
+
+    for general_element in xml_root.findall("./general"):
+        name_element = general_element.find('name')
+        name_element.text = name_element.text + string
+
+    return ET.tostring(xml_root)
+
+
+def timestamp_note(xml_str, append=True):
+    d = datetime.datetime.now().strftime("%Y.%m.%d_%H:%M:%S")
+
+    if append:
+        return append_additional_note(d, xml_str)
+
+    return set_additional_note(d, xml_str)
+
+
+def append_additional_note(string, xml_str):
+    xml_root = ET.fromstring(xml_str)
+
+    for ext_attrs in xml_root.findall("./extension_attributes"):
+        for ext_attr in ext_attrs:
+
+            if ext_attr.find('name').text == "Additional Item Notes" or ext_attr.find('id').text == "45":
+                value_element = ext_attr.find('value')
+                value_element.text = value_element.text + string
+
+    return ET.tostring(xml_root)
+
+def set_additional_note(string, xml_str):
+    xml_root = ET.fromstring(xml_str)
+
+    for ext_attrs in xml_root.findall("./extension_attributes"):
+        for ext_attr in ext_attrs:
+            if ext_attr.find('id').text == "45":
+                value_element = ext_attr.find('value')
+                value_element.text = string
+
+    return ET.tostring(xml_root)
+
+
+def xml_to_string(xml_file):
+    xml_tree = ET.parse(xml_file)
+    xml_root = xml_tree.getroot()
+    return ET.tostring(xml_root)
 
 
 def update_managed_status(status, xml_file):
