c78561253bf78ec3cae4f6d602bc61f04683baf7
Thackery Archuletta <t.archuletta@utah.edu>
2019-02-07 17:05:21 -0700

    Using .format() for strings concatenation.

--- controller.py
+++ controller.py
@@ -1,11 +1,12 @@
 class Controller(object):
     def set_to_middle(self, window):
+        # Updates window info to current window state
+        window.update_idletasks()
+
         # Gets computer screen width and height
         screen_width = window.winfo_screenwidth()
         screen_height = window.winfo_screenheight()
 
-        # Updates window info to current window state
-        window.update_idletasks()
-
         # Sets window position
-        window.geometry('+%d+%d' % (screen_width / 2 - window.winfo_width() / 2, screen_height / 4))
+        # window.geometry('+%d+%d' % (screen_width / 2 - window.winfo_width(), screen_height / 4))
+        window.geometry('+{}+{}'.format(screen_width / 2 - window.winfo_width()/2, screen_height / 4))
