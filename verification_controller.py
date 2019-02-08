8c9a8c658fbcd5fd10887e390b7f3483fe1c2545
Thackery Archuletta <t.archuletta@utah.edu>
2019-02-07 17:17:16 -0700

    Only used when no matches are found since no comparison with the JSS is needed. When a comparison is needed, see DualVerifyController/View.

--- verification_controller.py
+++ verification_controller.py
@@ -1,47 +1,45 @@
-from entry_controller_base import EntryController
+from entry_controller import EntryController
 import inspect
 from management_tools import loggers
 import os
+from verification_view import VerifyView
 
 
 class VerificationController(EntryController):
-    def __init__(self, master, computer, verify_params):
-        super(VerificationController, self).__init__(master, computer)
+    def __init__(self, master, computer, verify_params, search_params):
+        view = VerifyView(master, self)
+        super(VerificationController, self).__init__(master, computer, view)
         self.proceed = None
         self.verify_params = verify_params
-        self.determine_widgets_from_config()
+        self.search_params = search_params
 
-    def proceed_operation(self):
-        self.store_verification_fields()
+        self.determine_user_entries()
+        self.populate_user_entries()
+
+        self.set_to_middle(view)
+
+    def proceed_operation(self, sender):
+        self.store_user_verification_fields()
         self.proceed = True
         self.entry_view.destroy()
 
-    def store_verification_fields(self):
+    def store_user_verification_fields(self):
         for param in self.verify_params.enabled:
             self.store_entry_field(param)
 
-    def populate_verification_fields(self):
-        none_filter = lambda x: "" if x is None else x
-
-        if 'barcode_1' in self.verify_params.enabled:
-            self.entry_view.barcode_1_entry.insert(0, "{}".format(none_filter(self.computer.barcode_1)))
-
-        if 'barcode_2' in self.verify_params.enabled:
-            self.entry_view.barcode_2_entry.insert(0, "{}".format(none_filter(self.computer.barcode_2)))
-
-        if 'asset_tag' in self.verify_params.enabled:
-            self.entry_view.asset_entry.insert(0, "{}".format(none_filter(self.computer.asset_tag)))
-
-        if 'computer_name' in self.verify_params.enabled:
-            self.entry_view.name_entry.insert(0, "{}".format(none_filter(self.computer.name)))
-
-    def determine_widgets_from_config(self):
+    def populate_user_entries(self):
         for param in self.verify_params.enabled:
-            self.determine_widget(param)
+            self.populate_user_entry(param)
+
+    def determine_user_entries(self):
+        self.entry_view.user_lbl.grid(row=0, column=1)
+        for param in self.verify_params.enabled:
+            self.determine_user_widget(param)
 
     def cancel_operation(self):
         self.proceed = False
         self.entry_view.destroy()
+
 
 
 cf = inspect.currentframe()
