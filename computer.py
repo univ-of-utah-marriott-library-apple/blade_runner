ef8ed85007b99ae3ec86266a53c44239d8fec89a
Thackery Archuletta <t.archuletta@utah.edu>
2019-02-21 16:59:59 -0700

    Documentation-commenting.
    Updated logging variable names.
    
    Renamed "prev" variables to "jss" variables.

--- computer.py
+++ computer.py
@@ -1,3 +1,25 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+################################################################################
+# Copyright (c) 2019 University of Utah Student Computing Labs.
+# All Rights Reserved.
+#
+# Author: Thackery Archuletta
+# Creation Date: Oct 2018
+# Last Updated: Feb 2019
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright notice appears in all copies and
+# that both that copyright notice and this permission notice appear
+# in supporting documentation, and that the name of The University
+# of Utah not be used in advertising or publicity pertaining to
+# distribution of the software without specific, written prior
+# permission. This software is supplied as is without expressed or
+# implied warranties of any kind.
+################################################################################
+
 import os
 import subprocess
 from management_tools import loggers
@@ -6,42 +28,65 @@ import inspect
 
 
 class Computer(object):
+    """Stores information about about a computer
+
+    Attributes
+        serial_number (str): Serial number of the computer
+        jss_id (int): JSS ID of the computer
+        barcode_1 (str): Barcode 1 of the computer
+        barcode_2 (str): Barcode 2 of the computer
+        asset_tag (str): Asset tag of the computer
+        name (str): Name of the computer
+        prev_name (str): JSS extension attribute
+        incorrect_barcode_1 (str): Incorrect barcode 1
+        incorrect_barcode_2 (str): Incorrect barcode 2
+        incorrect_asset (str): Incorrect asset tag
+        incorrect_serial (str): Incorrect serial number
+        jss_barcode_1 (str): Barcode 1 record from JSS
+        jss_barcode_2 (str): Barcode 2 record from JSS
+        jss_asset_tag (str): Asset tag record from JSS
+        jss_serial_number (str): Serial number record from JSS
+
+    """
     def __init__(self):
+        """ Initialize the Computer object. Stores information relative to the computer.
+
+        Intended to be used as the model of the computer that Blade-Runner is running on.
+        """
         self.serial_number = None
         self.jss_id = None
         self.barcode_1 = None
         self.barcode_2 = None
         self.asset_tag = None
         self.name = None
-        self.prev_computer_name = None
-        self.needs_enroll = None
+        self.prev_name = None
         self.incorrect_barcode_1 = None
         self.incorrect_barcode_2 = None
         self.incorrect_asset = None
         self.incorrect_serial = None
-        self.prev_barcode_1 = None
-        self.prev_barcode_2 = None
-        self.prev_asset_tag = None
-        self.prev_name = None
-        self.prev_serial_number = None
+        self.jss_barcode_1 = None
+        self.jss_barcode_2 = None
+        self.jss_asset_tag = None
+        self.jss_serial_number = None
 
     def get_serial(self):
         """Gets and returns serial number from the computer"""
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         logger.info("get_serial" + ": activated")
-
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Gets raw data that contains the serial number
         serial_request_raw = subprocess.check_output(["system_profiler", "SPHardwareDataType"])
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Gets serial number from the raw data
-        serial_number_local = re.findall('Serial Number .system.: (.*)', serial_request_raw)[0]
+        serial = re.findall('Serial Number .system.: (.*)', serial_request_raw)[0]
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         logger.info("get_serial" + ": succeeded")
-        return serial_number_local
+        return serial
 
-
+# Start logging.
 cf = inspect.currentframe()
-filename = inspect.getframeinfo(cf).filename
-filename = os.path.basename(filename)
-filename = os.path.splitext(filename)[0]
-logger = loggers.FileLogger(name=filename, level=loggers.DEBUG)
+abs_file_path = inspect.getframeinfo(cf).filename
+basename = os.path.basename(abs_file_path)
+lbasename = os.path.splitext(basename)[0]
+logger = loggers.FileLogger(name=lbasename, level=loggers.DEBUG)
+logger.debug("{} logger started.".format(lbasename))
