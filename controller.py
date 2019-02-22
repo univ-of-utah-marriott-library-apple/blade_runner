9bcc00accde5c6281e6c211b34f8581af4e8874a
Thackery Archuletta <t.archuletta@utah.edu>
2019-02-21 17:00:32 -0700

    Documentation/commenting.

--- controller.py
+++ controller.py
@@ -1,12 +1,45 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+################################################################################
+# Copyright (c) 2019 University of Utah Student Computing Labs.
+# All Rights Reserved.
+#
+# Author: Thackery Archuletta
+# Creation Date: Oct 2018
+# Last Updated: Feb 2019
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright notice appears in all copies and
+# that both that copyright notice and this permission notice appear
+# in supporting documentation, and that the name of The University
+# of Utah not be used in advertising or publicity pertaining to
+# distribution of the software without specific, written prior
+# permission. This software is supplied as is without expressed or
+# implied warranties of any kind.
+################################################################################
+
+
 class Controller(object):
-    def set_to_middle(self, window):
+    """Parent class for controller objects."""
+
+    def _set_to_middle(self, window):
+        """Sets a Tkinter window to the center of the screen.
+
+        Args:
+            window: Tkinter window.
+
+        Returns:
+            void
+        """
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Updates window info to current window state
         window.update_idletasks()
-
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Gets computer screen width and height
         screen_width = window.winfo_screenwidth()
         screen_height = window.winfo_screenheight()
-
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Sets window position
-        # window.geometry('+%d+%d' % (screen_width / 2 - window.winfo_width(), screen_height / 4))
         window.geometry('+{}+{}'.format(screen_width / 2 - window.winfo_width()/2, screen_height / 4))
