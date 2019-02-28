84d5447098fae0da1d739e4c83cd6ce2bedfc68d
colossatr0n <29556317+colossatr0n@users.noreply.github.com>
2019-02-28 12:17:17 -0700

    Removed deprecated code.
    Updated variable references.
    
    Removed set_correct_entries_to_none. Contributed to unexpected behavior and isn't needful.
    
    Slack message now contains serial number.
    
    Updated logging.

--- main_controller.py
+++ main_controller.py
@@ -14,7 +14,7 @@ import user_xml_updater as user
 from params import SearchParams, VerifyParams
 from search_controller import SearchController
 from verification_controller import VerificationController
-from dual_verify_controller import DualVerificationController
+from dual_verify_controller import DualVerifyController
 from controller import Controller
 
 # TODO Interface with a Trello board and dynamically create lists for the DEP
@@ -42,10 +42,9 @@ class MainController(Controller):
         self.verify_params = VerifyParams(verify_params)
         self.search_params = SearchParams(search_params)
 
-        self.refocus()
-
     def run(self):
         self.main_view = MainView(self.root, self)
+        self._set_to_middle(self.main_view)
         self.refocus()
         self.main_view.mainloop()
 
@@ -107,16 +106,6 @@ class MainController(Controller):
         elif self.search_params.all_searched():
             self.process_logic()
 
-    # def next_input_old(self):
-    #     for param in self.search_params:
-    #         if not self.search_params.was_searched(param):
-    #             if self.computer.jss_id is None:
-    #                 if self.proceed is True:
-    #                     self.determine_input_type(param)
-    #                 else:
-    #                     return
-    #     self.restart()
-
     def act(self, input_type):
         self.determine_input_type(input_type)
 
@@ -131,20 +120,19 @@ class MainController(Controller):
         if self.search_params.exists_match():
             if self.search_params.search_count >= 1:
 
-                self.computer.prev_barcode_1 = self.jss_server.get_barcode_1(self.computer.jss_id)
-                self.computer.prev_barcode_2 = self.jss_server.get_barcode_2(self.computer.jss_id)
-                self.computer.prev_asset_tag = self.jss_server.get_asset_tag(self.computer.jss_id)
-                self.computer.prev_serial_number = self.jss_server.get_serial(self.computer.jss_id)
+                self.computer.jss_barcode_1 = self.jss_server.get_barcode_1(self.computer.jss_id)
+                self.computer.jss_barcode_2 = self.jss_server.get_barcode_2(self.computer.jss_id)
+                self.computer.jss_asset_tag = self.jss_server.get_asset_tag(self.computer.jss_id)
+                self.computer.jss_serial_number = self.jss_server.get_serial(self.computer.jss_id)
                 self.computer.prev_name = self.jss_server.get_computer_name(self.computer.jss_id)
 
-                logger.debug("Previous barcode_1: {}".format(self.computer.prev_barcode_1))
-                logger.debug("Previous barcode_2: {}".format(self.computer.prev_barcode_2))
-                logger.debug("Previous asset_tag: {}".format(self.computer.prev_asset_tag))
-                logger.debug("Previous serial_number: {}".format(self.computer.prev_serial_number))
+                logger.debug("Previous barcode_1: {}".format(self.computer.jss_barcode_1))
+                logger.debug("Previous barcode_2: {}".format(self.computer.jss_barcode_2))
+                logger.debug("Previous asset_tag: {}".format(self.computer.jss_asset_tag))
+                logger.debug("Previous serial_number: {}".format(self.computer.jss_serial_number))
                 logger.debug("Previous name: {}".format(self.computer.prev_name))
 
-                self.verify_controller = DualVerificationController(self.main_view, self.computer, self.verify_params,
-                                                                self.search_params)
+                self.verify_controller = DualVerifyController(self.main_view, self.computer, self.verify_params)
         else:
             self.verify_controller = VerificationController(self.main_view, self.computer, self.verify_params, self.search_params)
 
@@ -200,7 +188,6 @@ class MainController(Controller):
                 self.restart()
                 return
 
-            self.set_correct_entries_to_none(self.computer)
             self.store_incorrect_records(self.computer)
 
             # Check if Managed
@@ -217,8 +204,9 @@ class MainController(Controller):
 
         # TODO Remove this for open source version. MacGroup only.
         # At this point, all computer fields that were the same are now none.
-        self.offboard_config = user.append_xml_str_name(self.computer.get_serial(), self.offboard_config)
+        self.offboard_config = user.append_name(self.computer.get_serial(), self.offboard_config)
         self.offboard_config = user.timestamp_note(self.offboard_config)
+        # ADD PREVIOUS NAME UPDATER HERE
 
         self.jss_server.push_xml_str_fields(self.offboard_config, self.computer.jss_id)
         # self.jss_server.push_xml_fields(os.path.join(self.private_dir, self.offboard_config), self.computer.jss_id)
@@ -232,7 +220,8 @@ class MainController(Controller):
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Notify slack channel that offboard tool has finished.
         if self.slack_data['slack_enabled'] == "True":
-            self.send_slack_message("Offboarding complete.")
+            # TODO add this to a config.
+            self.send_slack_message("Offboarding complete. Serial {}".format(self.computer.serial_number))
         # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
         # Prepare document for printing
         doc = JssDoc(self.jss_server, self.computer.jss_id, self.computer)
@@ -251,19 +240,19 @@ class MainController(Controller):
 
     def store_incorrect_records(self, computer):
         # Check to see what fields changed after the user updated the fields through the entry view window.
-        if computer.barcode_1 != computer.prev_barcode_1:
-            computer.incorrect_barcode_1 = computer.prev_barcode_1
+        if computer.barcode_1 != computer.jss_barcode_1:
+            computer.incorrect_barcode_1 = computer.jss_barcode_1
             logger.debug("barcode_1 {} is incorrect.".format(computer.incorrect_barcode_1))
 
-        if computer.barcode_2 != computer.prev_barcode_2:
-            computer.incorrect_barcode_2 = computer.prev_barcode_2
+        if computer.barcode_2 != computer.jss_barcode_2:
+            computer.incorrect_barcode_2 = computer.jss_barcode_2
             logger.debug("barcode_2 {} is incorrect.".format(computer.incorrect_barcode_2))
 
-        if computer.asset_tag != computer.prev_asset_tag:
-            computer.incorrect_asset = computer.prev_asset_tag
+        if computer.asset_tag != computer.jss_asset_tag:
+            computer.incorrect_asset = computer.jss_asset_tag
             logger.debug("asset_tag {} is incorrect.".format(computer.incorrect_asset))
 
-        if computer.prev_serial_number != computer.get_serial():
+        if computer.jss_serial_number != computer.get_serial():
             computer.serial_number = computer.get_serial()
             computer.incorrect_serial = self.jss_server.get_serial(computer.jss_id)
             logger.debug("JSS serial {} is incorrect.".format(computer.incorrect_serial))
@@ -271,58 +260,24 @@ class MainController(Controller):
     def set_correct_entries_to_none(self, computer):
         # Check to see what fields changed after the user updated the fields through the entry view window.
 
-        if computer.barcode_1 == computer.prev_barcode_1:
+        if computer.barcode_1 == computer.jss_barcode_1:
             logger.debug("barcode_1 {} is correct.".format(computer.barcode_1))
             computer.barcode_1 = None
 
-        if computer.barcode_2 == computer.prev_barcode_2:
+        if computer.barcode_2 == computer.jss_barcode_2:
             logger.debug("barcode_2 {} is correct.".format(computer.barcode_2))
             computer.barcode_2 = None
 
-        if computer.asset_tag == computer.prev_asset_tag:
+        if computer.asset_tag == computer.jss_asset_tag:
             logger.debug("asset_tag {} is correct.".format(computer.asset_tag))
             computer.asset_tag = None
 
-        if computer.serial_number == computer.prev_serial_number:
+        if computer.serial_number == computer.jss_serial_number:
             logger.debug("Serial {} is correct.".format(computer.serial_number))
             computer.serial_number = None
 
         if computer.name == computer.prev_name:
             computer.name = None
-
-    # def keep_unique_entries(self):
-    #     # Check to see what fields changed after the user updated the fields through the entry view window.
-    #     if self.computer.barcode_1 == self.computer.prev_barcode_1:
-    #         logger.debug("barcode_1 {} is correct.".format(self.computer.barcode_1))
-    #         self.computer.barcode_1 = None
-    #     else:
-    #         self.computer.incorrect_barcode_1 = self.computer.prev_barcode_1
-    #         logger.debug("barcode_1 {} is incorrect.".format(self.computer.incorrect_barcode_1))
-    #
-    #     if self.computer.barcode_2 == self.computer.prev_barcode_2:
-    #         logger.debug("barcode_2 {} is correct.".format(self.computer.barcode_2))
-    #         self.computer.barcode_2 = None
-    #     else:
-    #         self.computer.incorrect_barcode_2 = self.computer.prev_barcode_2
-    #         logger.debug("barcode_2 {} is incorrect.".format(self.computer.incorrect_barcode_2))
-    #
-    #     if self.computer.asset_tag == self.computer.prev_asset_tag:
-    #         logger.debug("asset_tag {} is correct.".format(self.computer.asset_tag))
-    #         self.computer.asset_tag = None
-    #     else:
-    #         self.computer.incorrect_asset = self.computer.prev_asset_tag
-    #         logger.debug("asset_tag {} is incorrect.".format(self.computer.incorrect_asset))
-    #
-    #     if self.jss_server.get_serial(self.computer.jss_id) == self.computer.get_serial():
-    #         logger.debug("Serial {} is correct.".format(self.computer.serial_number))
-    #         self.computer.serial_number = None
-    #     else:
-    #         self.computer.serial_number = self.computer.get_serial()
-    #         self.computer.incorrect_serial = self.jss_server.get_serial(self.computer.jss_id)
-    #         logger.debug("JSS serial {} is incorrect.".format(self.computer.incorrect_serial))
-    #
-    #     if self.computer.name == self.computer.prev_name:
-    #         self.computer.name = None
 
     def send_slack_message(self, message):
         current_ip = socket.gethostbyname(socket.gethostname())
@@ -350,9 +305,11 @@ abs_file_path = inspect.getframeinfo(cf).filename
 basename = os.path.basename(abs_file_path)
 lbasename = os.path.splitext(basename)[0]
 logger = loggers.FileLogger(name=lbasename, level=loggers.DEBUG)
+logger.debug("{} logger started.".format(lbasename))
 
 
 def main():
+    # TODO uncomment sudo check for production
     # if os.geteuid() != 0:
     #     raise SystemExit("Must be run as root.")
     root = tk.Tk()
@@ -363,16 +320,11 @@ def main():
     jss_server_plist = os.path.join(blade_runner_dir, "private/jss_server_config.plist")
     jss_server_data = plistlib.readPlist(jss_server_plist)
     jss_server = JssServer(**jss_server_data)
-    logger.debug(jss_server.jss_url)
+    logger.debug(jss_server._jss_url)
 
     # Read from Slack config plist to set up Slack notifications
     slack_plist = os.path.join(blade_runner_dir, "private/slack_config.plist")
     slack_data = plistlib.readPlist(slack_plist)
-    current_ip = socket.gethostbyname(socket.gethostname())
-    bot = IWS(slack_data['slack_url'], bot_name=current_ip, channel=slack_data['slack_channel'])
-
-    # Read from the offboard config plist to set up which fields will be offboarded
-    offboard_xml = "private/offboard_config.xml"
 
     verify_config = os.path.join(blade_runner_dir, "private/verify_config.plist")
     verify_data = plistlib.readPlist(verify_config)
