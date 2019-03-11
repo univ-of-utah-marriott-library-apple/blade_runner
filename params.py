1ac95b9a8fae401deadbf390889c7c848195ba25
Thackery Archuletta <t.archuletta@utah.edu>
2019-03-11 12:26:52 -0600

    Docstring/comments for _values_to_bools().
    Updated header comment.

--- params.py
+++ params.py
@@ -1,23 +1,54 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+################################################################################
+# Copyright (c) 2019 University of Utah Student Computing Labs.
+# All Rights Reserved.
+#
+# Author: Thackery Archuletta
+# Creation Date: Oct 2018
+# Last Updated: Feb 2019
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright notice appears in all copies and
+# that both that copyright notice and this permission notice appear
+# in supporting documentation, and that the name of The University
+# of Utah not be used in advertising or publicity pertaining to
+# distribution of the software without specific, written prior
+# permission. This software is supplied as is without expressed or
+# implied warranties of any kind.
+################################################################################
+
+
 class Params(object):
 
     def _values_to_bools(self, dictionary):
-        """
-        Converts values, one level deep in a dictionary, that are string booleans to booleans, e.g.,
+        """Converts values, one level deep in a dictionary, that are string booleans to booleans, e.g.,
 
             "False" -> False
                 or
             "false" -> False
 
-        :param dictionary: Dictionary.
-        :return: Copy of dictionary.
+        Args:
+            dictionary (dict): Dictionary with values of string booleans.
+
+        Returns:
+            Copy of the dictionary with boolean values instead of string boolean values.
         """
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>x
+        # Deep copy of dictionary.
         dct = dict(dictionary)
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # For every key, convert the string boolean into a boolean.
         for key in dictionary:
             if isinstance(dct[key], str):
                 if dct[key].lower() == "false":
                     dct[key] = False
                 elif dct[key].lower() == "true":
                     dct[key] = True
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # Return the copy of the dictionary.
         return dct
 
     def _remove_keys_with_value(self, value, dictionary):
