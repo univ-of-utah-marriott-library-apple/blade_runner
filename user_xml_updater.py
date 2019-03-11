41f3d38559bd76c7895dbd9093084c909899d71f
Thackery Archuletta <t.archuletta@utah.edu>
2019-03-11 14:22:01 -0600

    Docstring/comments for xml_file_append_name().

--- user_xml_updater.py
+++ user_xml_updater.py
@@ -1,15 +1,52 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+################################################################################
+# Copyright (c) 2019 University of Utah Student Computing Labs.
+# All Rights Reserved.
+#
+# Author: Thackery Archuletta
+# Creation Date: Oct 2018
+# Last Updated: Feb 2019
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright notice appears in all copies and
+# that both that copyright notice and this permission notice appear
+# in supporting documentation, and that the name of The University
+# of Utah not be used in advertising or publicity pertaining to
+# distribution of the software without specific, written prior
+# permission. This software is supplied as is without expressed or
+# implied warranties of any kind.
+################################################################################
+
 import xml.etree.cElementTree as ET
 import datetime
 
 
 def xml_file_append_name(string, xml_file):
-    xml_tree = ET.parse(xml_file)
-    xml_root = xml_tree.getroot()
+    """Appends the name tag in the XML file with the given string.
 
+    Args:
+        string (str): String that is appended to the name.
+        xml_file (str): File path to XML file.
+
+    Returns:
+        void
+    """
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    # Parse XML file into an XML tree.
+    xml_tree = ET.parse(xml_file)
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    # Get the root of the XML tree.
+    xml_root = xml_tree.getroot()
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    # Find the "name" tag in "general" and append its value with the string input.
     for general_element in xml_root.findall("./general"):
         name_element = general_element.find('name')
         name_element.text = name_element.text + string
-
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    # Return an XML string.
     return ET.tostring(xml_root)
 
 
