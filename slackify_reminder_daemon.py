4c9f9e20e701638b31be3af852b1ddee43efe064
Thackery Archuletta <t.archuletta@utah.edu>
2019-03-11 13:32:43 -0600

    Added header comment.

--- slackify_reminder_daemon.py
+++ slackify_reminder_daemon.py
@@ -1,3 +1,25 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+################################################################################
+# Copyright (c) 2019 University of Utah Student Computing Labs.
+# All Rights Reserved.
+#
+# Author: Thackery Archuletta
+# Creation Date: Oct 2018
+# Last Updated: Feb 2019
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright notice appears in all copies and
+# that both that copyright notice and this permission notice appear
+# in supporting documentation, and that the name of The University
+# of Utah not be used in advertising or publicity pertaining to
+# distribution of the software without specific, written prior
+# permission. This software is supplied as is without expressed or
+# implied warranties of any kind.
+################################################################################
+
 from daemon import daemon
 import time
 import subprocess
@@ -14,6 +36,7 @@ def slackify_reminder():
     script_path = os.path.join(script_dir, 'slackify.py')
     channel = slack_data['slack_channel']
     message = 'Reminder: Blade-Runner finished.'
+
     while True:
         hour = time.localtime().tm_hour
         if 9 <= hour < 18:
@@ -24,7 +47,7 @@ def slackify_reminder():
 
 def run_daemon():
     print("Running Slackify daemon.")
-    with daemon.DaemonContext(stdout=sys.stdout, stderr=sys.stdout, pidfile=filelock.FileLock('/tmp/spam.pid', timeout=0)):
+    with daemon.DaemonContext(stdout=sys.stdout, stderr=sys.stdout, pidfile=filelock.FileLock('/tmp/slackify.pid', timeout=0)):
         slackify_reminder()
 
 
