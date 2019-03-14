7876edeb093ac364feb93096358802b661cae490
Thackery Archuletta <t.archuletta@utah.edu>
2019-03-14 14:08:12 -0600

    Unit test for manually testing the GUI with test configs.

---
+++ test_gui_manual.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import unittest
+import os
+import plistlib
+from jss_server import JssServer
+import inspect
+import Tkinter as tk
+from main_controller import MainController
+
+
+class TestBladeRunner(unittest.TestCase):
+
+    def setUp(self):
+        cf = inspect.currentframe()
+
+        root = tk.Tk()
+        root.withdraw()
+
+        abs_file_path = inspect.getframeinfo(cf).filename
+        self.blade_runner_dir = os.path.dirname(abs_file_path)
+        jss_server_plist = os.path.join(self.blade_runner_dir, "private/test/jss_server_config.plist")
+        jss_server_data = plistlib.readPlist(jss_server_plist)
+        jss_server = JssServer(**jss_server_data)
+
+        self.slack_plist = os.path.join(self.blade_runner_dir, "private/test/slack_config.plist")
+        self.slack_data = plistlib.readPlist(self.slack_plist)
+
+        self.verify_config = os.path.join(self.blade_runner_dir, "private/test/verify_params_config_all_enabled.plist")
+        self.verify_data = plistlib.readPlist(self.verify_config)
+
+        self.search_params_config = os.path.join(self.blade_runner_dir, "private/test/search_params_config_all_enabled.plist")
+        self.search_params = plistlib.readPlist(self.search_params_config)
+
+        self.br = MainController(root, jss_server, self.slack_data, self.verify_data, self.search_params)
+
+
+class TestServerGUIManual(TestBladeRunner):
+
+    def setUp(self):
+        super(TestServerGUIManual, self).setUp()
+
+    def test_manual(self):
+        self.br.run()
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
