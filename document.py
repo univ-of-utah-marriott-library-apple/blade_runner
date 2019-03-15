390473e2820d46a9af72ccebec5bc0658e7000dd
colossatr0n <29556317+colossatr0n@users.noreply.github.com>
2019-03-14 19:10:10 -0600

    document.py is to separate concerns in JssDoc and secure_erase_internals.py. Right now, they both contain similar functions for creating documents. The plan is to have them both import the same functions from document.py.

---
+++ document.py
@@ -0,0 +1,95 @@
+import os
+import inspect
+from management_tools import loggers
+import subprocess
+import webbrowser
+
+
+def create_html(content, filepath):
+    if not filepath.endswith(".html"):
+        filepath = "{}.html".format(filepath)
+
+    with open(filepath, "w+") as f:
+        f.write(content)
+
+
+def open_html(filepath):
+    """Open the html file in Safari.
+
+    Returns:
+        void
+    """
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    logger.info("open_html: started")
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    try:# open HTML file in Safari.
+        webbrowser.get('macosx').open("file://" + filepath)
+    except Exception as e:
+        logger.error("Couldn't open webrowser. {}".format(e))
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    logger.info("open_html: finished")
+
+
+def html_to_pdf(html):
+    """Convert HTML file to PDF. Prints only the first page, which is the "-P 1" option.
+
+    Returns:
+        void
+    """
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    logger.info("Converting HTML to PDF")
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    pdf = "{}.pdf".format(html)
+
+    try:# convert HTML to PDF.
+        with open(pdf, 'w+') as pdfout:
+            subprocess.call(['/usr/sbin/cupsfilter', "-P", "1", html], stdout=pdfout,
+                            stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        logger.error(e.output)
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    logger.info("Converting HTML to PDF finished.")
+
+
+def print_pdf_to_default(pdf):
+    """Print PDF file to default printer.
+
+    Returns:
+        void
+    """
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    logger.info("print_pdf_to_default: started")
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    # Build AppleScript to print PDF to default printer from Preview.
+    script = r'''
+    set theFile to POSIX path of "{}"
+    do shell script("open " & theFile) 
+    tell application "Preview"
+        delay 2
+        print the front document
+    end tell
+            '''.format(pdf)
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    # Write AppleScript text to a bash file.
+    with open("/tmp/print.sh", "w+") as f:
+        f.write(script)
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    try:# run the AppleScript with osascript.
+        cmd = ['/usr/bin/osascript', '/tmp/print.sh']
+        subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        logger.info("Print successful.")
+        # Append file name with "printed".
+        pre_ext, ext = os.path.splitext(pdf)
+        os.rename(pdf, "{}_printed.pdf".format(pre_ext))
+    except subprocess.CalledProcessError as e:
+        logger.error(str(e.output))
+        logger.error("Document didn't print. Make sure a default printer has been configured.")
+    # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    logger.info("print_pdf_to_default: finished")
+
+
+cf = inspect.currentframe()
+abs_file_path = inspect.getframeinfo(cf).filename
+basename = os.path.basename(abs_file_path)
+lbase = os.path.splitext(basename)[0]
+logger = loggers.FileLogger(name=lbase, level=loggers.DEBUG)
