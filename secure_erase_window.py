8213f4eb58648abbc4f443a3498417880e6cd754
colossatr0n <29556317+colossatr0n@users.noreply.github.com>
2019-03-15 22:29:57 -0600

    It works!! Finally, finally, finally. dynamic_print can now retrieve and print in place the secure erase progress line. Before, I wasn't able to get the progress line. Had to call the secure erase command in secure_erase_internals.py without polling. Now it's just a simple Popen command. It finally works. Now all the output from secure_erase_internals.py can be printed to the text area of the tkinter window. Thank goodness.

--- secure_erase_window.py
+++ secure_erase_window.py
@@ -1,5 +1,4 @@
 from Tkinter import *
-import subprocess
 import pexpect
 import tkSimpleDialog
 
@@ -25,37 +24,30 @@ class ProcessWindow(Toplevel):
         self.after(1000, lambda: self.sudo_process(cmd, self.text))
 
     def sudo_process(self, cmd, text):
-        root = Tk()
-        root.withdraw()
-
         try:
-            child = pexpect.spawn('bash', cmd)
+            child = pexpect.spawn('bash', cmd, use_poll=True)
 
             while True:
-                result = child.expect(['SECURE ERASE INTERNALS', 'attempts', pexpect.EOF, pexpect.TIMEOUT, 'Password:', "Started"])
+                result = child.expect(['SECURE ERASE INTERNALS', 'attempts', pexpect.EOF, pexpect.TIMEOUT, 'Password:'])
                 print(result)
                 if result == 4:
-                    password = tkSimpleDialog.askstring("Password", "Enter admin password:", show='*', parent=root)
+                    self.focus_force()
+                    password = tkSimpleDialog.askstring("Password", "Enter admin password:", show='*', parent=self)
                     child.sendline(password)
                 elif result == 1:
                     msg = "Could not execute command. Password was incorrect."
                     return Results(False, msg)
                 elif result == 2:
-                    msg = "Reached end of output. Process may not have been executed."
+                    msg = "Reached end of output. Process may or may not have been executed."
                     return Results(False, msg)
                 elif result == 3:
                     msg = "Password prompt timed out."
                     return Results(False, msg)
                 elif result == 0:
-                    self.normal_print(child, text)
-                elif result == 5:
-                    text.insert(END, "\n " + child.after + " " + child.readline())
-                    self.in_place_print(child, text)
-                    line = self.normal_print(child, text)
-                    if "0" not in line:
-                        return Results(False, "secure_erase_internals.py returned a non-zero exit code")
-                    msg = "Process was successful."
-                    return Results(True, msg)
+                    last_line = self.dynamic_print(child, text)
+                    if "0" not in last_line:
+                        return Results(False, "There was an error.")
+                    return Results(True, "Secure erase was successful!")
                 else:
                     msg = "Unknown error occurred."
                     return Results(False, msg)
@@ -66,40 +58,54 @@ class ProcessWindow(Toplevel):
     def _close_btn_clicked(self):
         self.destroy()
 
-    def normal_print(self, child, text):
-        for line in child:
-            if "SECURE ERASING" in line: break
-            if line != "":
+    def dynamic_print(self, child, text):
+        prev_result = None
+        while child.isalive():
+            line = ""
+            result = child.expect(["\[[^K][^\]]*\]", "\n", "\[K"])
+            if result == 0:
+                line = child.after
+                print(line)
+                pos = text.index("end-1c linestart")
+                text.delete(pos, END)
+                text.insert(END, "\n")
+                text.insert(END, line.strip())
+                text.update()
+                text.see("end")
+            elif result == 1:
+                print(child.before)
+                if prev_result == 0 or prev_result == 2:
+                    text.insert(END, "\n")
+                line = child.before
                 line = line.strip()
                 text.insert(END, line + "\n")
                 text.update()
                 text.see("end")
+
+            prev_result = result
         return line
 
-    def in_place_print(self, child, text):
-        while True:
-            result = child.expect(["\[", pexpect.EOF, "Finished", "Started"])
-            if result == 0:
-                if "Started" not in child.before and "K" not in child.before:
-                    line = "[ {}".format(child.before)
-                    pos = text.index("end-1c linestart")
-                    text.delete(pos, END)
-                    text.insert(END, "\n")
-                    text.insert(END, line.strip())
-                    text.update()
-                    text.see("end")
-            elif result == 1:
-                break
-            elif result == 2:
-                print("\n " + child.after + " " + child.readline().strip())
-                break
-            elif result == 3:
-                text.insert(END, child.after + " " + child.readline().strip())
-            else:
-                break
-
-
-
+    # def in_place_print(self, child, text):
+    #     while True:
+    #         result = child.expect(["\[", pexpect.EOF, "Finished", "Started"])
+    #         if result == 0:
+    #             if "Started" not in child.before and "K" not in child.before:
+    #                 line = "[ {}".format(child.before)
+    #                 pos = text.index("end-1c linestart")
+    #                 text.delete(pos, END)
+    #                 text.insert(END, "\n")
+    #                 text.insert(END, line.strip())
+    #                 text.update()
+    #                 text.see("end")
+    #         elif result == 1:
+    #             break
+    #         elif result == 2:
+    #             print("\n " + child.after + " " + child.readline().strip())
+    #             break
+    #         elif result == 3:
+    #             text.insert(END, child.after + " " + child.readline().strip())
+    #         else:
+    #             break
 
 class Results(object):
 
