5964b51df75a5d55fb6817871bbf568bf0b0526a
Thackery Archuletta <t.archuletta@utah.edu>
2019-03-26 11:40:08 -0600

    Updated class name.
    _set_to_middle() added. Window is now set to middle of screen.
    
    Added message for when user cancels operation.

--- secure_erase_window.py
+++ secure_erase_window.py
@@ -3,12 +3,13 @@ import pexpect
 import tkSimpleDialog
 
 
-class ProcessWindow(Toplevel):
+class SecureEraseWindow(Toplevel):
 
     def __init__(self, cmd, master):
         Toplevel.__init__(self, master)
         self.protocol('WM_DELETE_WINDOW', self._close_btn_clicked)
         self.title("Secure Erase Internals Output")
+        self.result = None
         self.frame = Frame(self, width=100, height=100)
         self.frame.pack(fill=None, expand=False)
         self.frame.grid_propagate(False)
@@ -21,39 +22,46 @@ class ProcessWindow(Toplevel):
         self.text['yscrollcommand'] = self.scrollbar.set
         self.text.pack()
         self.text.insert(END, "Waiting for authentication...")
+        self._set_to_middle(self)
         self.after(1000, lambda: self.sudo_process(cmd, self.text))
 
     def sudo_process(self, cmd, text):
         try:
             child = pexpect.spawn('bash', cmd, use_poll=True)
 
-            while True:
-                result = child.expect(['SECURE ERASE INTERNALS', 'attempts', pexpect.EOF, pexpect.TIMEOUT, 'Password:'])
-                print(result)
-                if result == 4:
+            while not self.result:
+                match = child.expect(['SECURE ERASE INTERNALS', 'attempts', pexpect.EOF, pexpect.TIMEOUT, 'Password:'])
+                if match == 4:
                     self.focus_force()
                     password = tkSimpleDialog.askstring("Password", "Enter admin password:", show='*', parent=self)
-                    child.sendline(password)
-                elif result == 1:
+                    if not password:
+                        self.result = Results(False, "User cancelled.")
+                        self.destroy()
+                    else:
+                        child.sendline(password)
+                elif match == 1:
                     msg = "Could not execute command. Password was incorrect."
+                    self.result = Results(False, msg)
+                    self.destroy()
+                elif match == 2:
+                    msg = "Reached end of output early. Process may or may not have been executed."
                     return Results(False, msg)
-                elif result == 2:
-                    msg = "Reached end of output. Process may or may not have been executed."
-                    return Results(False, msg)
-                elif result == 3:
+                elif match == 3:
                     msg = "Password prompt timed out."
-                    return Results(False, msg)
-                elif result == 0:
+                    self.result = Results(False, msg)
+                elif match == 0:
                     last_line = self.dynamic_print(child, text)
                     if "0" not in last_line:
-                        return Results(False, "There was an error.")
-                    return Results(True, "Secure erase was successful!")
+                        self.result = Results(False, "There was an error.")
+                    else:
+                        self.result = Results(True, "Secure erase was successful!")
                 else:
                     msg = "Unknown error occurred."
-                    return Results(False, msg)
+                    self.result = Results(False, msg)
         except Exception as e:
             msg = "Unknown error.".format(e)
-            return Results(False, msg)
+            self.result = Results(False, msg)
+            raise
 
     def _close_btn_clicked(self):
         self.destroy()
@@ -85,27 +93,26 @@ class ProcessWindow(Toplevel):
             prev_result = result
         return line
 
-    # def in_place_print(self, child, text):
-    #     while True:
-    #         result = child.expect(["\[", pexpect.EOF, "Finished", "Started"])
-    #         if result == 0:
-    #             if "Started" not in child.before and "K" not in child.before:
-    #                 line = "[ {}".format(child.before)
-    #                 pos = text.index("end-1c linestart")
-    #                 text.delete(pos, END)
-    #                 text.insert(END, "\n")
-    #                 text.insert(END, line.strip())
-    #                 text.update()
-    #                 text.see("end")
-    #         elif result == 1:
-    #             break
-    #         elif result == 2:
-    #             print("\n " + child.after + " " + child.readline().strip())
-    #             break
-    #         elif result == 3:
-    #             text.insert(END, child.after + " " + child.readline().strip())
-    #         else:
-    #             break
+    def _set_to_middle(self, window):
+        """Sets a Tkinter window to the center of the screen.
+
+        Args:
+            window: Tkinter window.
+
+        Returns:
+            void
+        """
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # Updates window info to current window state
+        window.update_idletasks()
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # Gets computer screen width and height
+        screen_width = window.winfo_screenwidth()
+        screen_height = window.winfo_screenheight()
+        # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # Sets window position
+        window.geometry('+{}+{}'.format(screen_width / 2 - window.winfo_width()/2, screen_height / 4))
+
 
 class Results(object):
 
@@ -119,5 +126,5 @@ if __name__ == "__main__":
     master.withdraw()
     cmd = ['-c', '/usr/bin/sudo python secure_erase_internals.py; echo "Return code: $?"']
 
-    window = ProcessWindow(cmd, master)
+    window = SecureEraseWindow(cmd, master)
     master.wait_window(window)
