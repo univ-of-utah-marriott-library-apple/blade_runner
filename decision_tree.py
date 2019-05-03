143eed7151392797d3e241e37f1906e6630c6d6d
Thackery Archuletta <t.archuletta@utah.edu>
2019-05-03 16:04:15 -0600

    Removed files.

--- decision_tree.py
+++
@@ -1,8 +0,0 @@
-
-
-class DecisionTree(object):
-    def __init__(self):
-        self.search_param = None
-        self.proceed = None
-        self.no_jss_id = None
-        self.needs_enrollment = None
