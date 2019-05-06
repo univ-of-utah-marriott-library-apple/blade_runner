0f9d2c5ec2885acb7c5a37d982b73ca31ea674c2
Thackery Archuletta <t.archuletta@utah.edu>
2019-05-06 14:54:05 -0600

    Updated paths.

--- test/test_gui_server_manual.py
+++ test/test_gui_server_manual.py
@@ -25,18 +25,18 @@ class TestGUIServerManual(unittest.TestCase):
         root.withdraw()
 
         abs_file_path = inspect.getframeinfo(cf).filename
-        self.blade_runner_dir = os.path.dirname(os.path.dirname(abs_file_path))
-        jss_server_plist = os.path.join(self.blade_runner_dir, "private/test/jss_server_config.plist")
+        self.blade_runner_dir = os.path.dirname(abs_file_path)
+        jss_server_plist = os.path.join(self.blade_runner_dir, "private/jamf_pro_configs/jamf_pro.plist")
         jss_server_data = plistlib.readPlist(jss_server_plist)
         jss_server = JssServer(**jss_server_data)
 
-        self.slack_plist = os.path.join(self.blade_runner_dir, "private/test/slack_config.plist")
+        self.slack_plist = os.path.join(self.blade_runner_dir, "private/slack_configs/slack.plist")
         self.slack_data = plistlib.readPlist(self.slack_plist)
 
-        self.verify_config = os.path.join(self.blade_runner_dir, "private/test/verify_params_config_all_enabled.plist")
+        self.verify_config = os.path.join(self.blade_runner_dir, "private/verify_params_configs/verify_params.plist")
         self.verify_data = plistlib.readPlist(self.verify_config)
 
-        self.search_params_config = os.path.join(self.blade_runner_dir, "private/test/search_params_config_all_enabled.plist")
+        self.search_params_config = os.path.join(self.blade_runner_dir, "private/search_params_configs/search_params.plist")
         self.search_params = plistlib.readPlist(self.search_params_config)
 
         self.br = MainController(root, jss_server, self.slack_data, self.verify_data, self.search_params)
