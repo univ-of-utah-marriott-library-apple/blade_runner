e5fcc15c42bf59d261efa217eb46b202db88f5ee
Thackery Archuletta <t.archuletta@utah.edu>
2019-05-15 12:46:00 -0600

    Added header comment.

--- test/test_gui_server_manual.py
+++ test/test_gui_server_manual.py
@@ -1,5 +1,34 @@
 #!/usr/bin/python
 
+# -*- coding: utf-8 -*-
+################################################################################
+# Copyright (c) 2019 University of Utah Student Computing Labs.
+# All Rights Reserved.
+#
+# Author: Thackery Archuletta
+# Creation Date: Oct 2018
+# Last Updated: March 2019
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright notice appears in all copies and
+# that both that copyright notice and this permission notice appear
+# in supporting documentation, and that the name of The University
+# of Utah not be used in advertising or publicity pertaining to
+# distribution of the software without specific, written prior
+# permission. This software is supplied as is without expressed or
+# implied warranties of any kind.
+################################################################################
+
+"""
+How to run:
+
+    sudo python test/test_gui_server_manual.py
+
+Current working directory must be Blade Runner.
+
+"""
+
 import os
 import sys
 import inspect
