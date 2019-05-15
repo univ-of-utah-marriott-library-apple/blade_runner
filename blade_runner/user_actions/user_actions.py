2ec16a565c2058efdb84df704f1910c0e18f546a
Thackery Archuletta <t.archuletta@utah.edu>
2019-05-15 12:38:59 -0600

    Added header comment.

--- blade_runner/user_actions/user_actions.py
+++ blade_runner/user_actions/user_actions.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+################################################################################
+# Copyright (c) 2019 University of Utah Student Computing Labs.
+# All Rights Reserved.
+#
+# Author: Thackery Archuletta
+# Creation Date: Oct 2018
+# Last Updated: March 2019
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright notice appears in all copies and
+# that both that copyright notice and this permission notice appear
+# in supporting documentation, and that the name of The University
+# of Utah not be used in advertising or publicity pertaining to
+# distribution of the software without specific, written prior
+# permission. This software is supplied as is without expressed or
+# implied warranties of any kind.
+################################################################################
+
 import xml_updater
 
 
