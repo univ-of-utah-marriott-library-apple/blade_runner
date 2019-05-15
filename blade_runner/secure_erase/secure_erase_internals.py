305817ca47539cdc600bfe61c02659f23a0f21bf
Thackery Archuletta <t.archuletta@utah.edu>
2019-05-15 12:45:47 -0600

    Added header comment.

--- blade_runner/secure_erase/secure_erase_internals.py
+++ blade_runner/secure_erase/secure_erase_internals.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
+
 # -*- coding: utf-8 -*-
+################################################################################
+# Copyright (c) 2019 University of Utah Student Computing Labs.
+# All Rights Reserved.
+#
+# Author: Thackery Archuletta
+# Creation Date: Oct 2018
+# Last Updated: March 2019
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright notice appears in all copies and
+# that both that copyright notice and this permission notice appear
+# in supporting documentation, and that the name of The University
+# of Utah not be used in advertising or publicity pertaining to
+# distribution of the software without specific, written prior
+# permission. This software is supplied as is without expressed or
+# implied warranties of any kind.
+################################################################################
 
 """This script is meant to be run off an external HD. Make sure that the management_tools module is in the correct
 folder on the external HD.
